1af2821a9fd95375c86460d8e22e8e3a76d76a0f
leeraper909 <lraper@linkupcomms.com>
2025-08-07 14:10:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("🚀 BlackGrid Elite Site Loaded.");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const score = document.querySelector(".score-value");
+  if (score) {
+    let current = 80;
+    const target = 94;
+    const interval = setInterval(() => {
+      if (current >= target) return clearInterval(interval);
+      current++;
+      score.textContent = current;
+    }, 80);
+  }
+});
